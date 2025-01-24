26f5f964dfa11c109c76f31dd15eef5cdf302361
SohalSawardekar <sohalsawardekar11@gmail.com>
2025-01-24 20:48:38 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:carrier_hive/widgets/wrapper/wrapper.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:carrier_hive/config/firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Carrier Hive',
+        home: Wrapper());
+  }
+}
